de94b3f26859e90e886d3b81010832094d9cc4a0
ishopjapan <thoitran2107@gmail.com>
2022-08-28 14:53:51 +0700

    tao cac package va cac lop trong model

---
+++ src/main/java/com/codegym/casestudy/model/Book.java
@@ -0,0 +1,93 @@
+package com.codegym.casestudy.model;
+
+public class Book {
+    private int id;
+    private String code;
+    private String name;
+    private double price;
+    private String bookType;
+    private String author;
+    private String description;
+
+    public Book() {
+    }
+
+    public Book(int id, String code, String name, double price, String bookType, String author, String description) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.price = price;
+        this.bookType = bookType;
+        this.author = author;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getBookType() {
+        return bookType;
+    }
+
+    public void setBookType(String bookType) {
+        this.bookType = bookType;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", bookType='" + bookType + '\'' +
+                ", author='" + author + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
