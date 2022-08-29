8bc8d7b78d3c147e0be083c85ccaac1a8029efbf
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-08-29 09:05:42 +0700

    update sql

--- DataSQL/CaseStudy3_UpdateData.sql
+++ DataSQL/CaseStudy3_UpdateData.sql
@@ -1,2 +1,4 @@
 insert into books (code, name, author, price, bookType, description)
-VALUES ('Manga01','test','test1')
+VALUES ('Manga01','test','test1',20.00,'Sách thiếu nhi','Nothing');
+
+select * from books;
