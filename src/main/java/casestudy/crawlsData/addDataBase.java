b6c57d25bb7e420a3ad7364c68c664e3f52a8ad8
ishopjapan <thoitran2107@gmail.com>
2022-08-30 11:08:55 +0700

    fix

--- src/main/java/casestudy/crawlsData/addDataBase.java
+++
@@ -1,6 +0,0 @@
-package casestudy.crawlsData;
-
-public class addDataBase {
-    private CrawlsDataBoos crawlsDataBoos = new CrawlsDataBoos();
-    private ConnectionJDBC connectionJDBC = new ConnectionJDBC();
-}
