14be0aaf9bcf8b6cc72700f47d71c123adc98bcd
ishopjapan <thoitran2107@gmail.com>
2022-08-30 09:52:12 +0700

    cao du lieu bang book

--- src/main/java/casestudy/crawlsData/CrawlsDataBoos.java
+++ src/main/java/casestudy/crawlsData/CrawlsDataBoos.java
@@ -16,27 +16,31 @@ public class CrawlsDataBoos {
             ArrayList<Element> elements = document.getElementsByClass("t-view");
             ArrayList<Element> elements1 = document.getElementsByClass("au-view");
             ArrayList<Element> elements2 = document.getElementsByClass("real-price");
-//            ArrayList<Element> elements3 = document.getElementsByClass("collection-title");
-            ArrayList<Element> elements4 = document.getElementsByClass("slimmage");
+            ArrayList<Element> elements3 = document.getElementsByClass("collection-title");
+            ArrayList<Element> elements4 = document.getElementsByClass("img-view");
 //            ArrayList<Element> elements5 = document.getElementsByClass("status");
             int id = 0;
+            int author_id = 0;
+            int category_id = 0;
             for (int i = 1; i <= 10; i++) {
                 id++;
+                String code = "";
+                String description = "Sach hay vai chuong";
                String name = elements.get(i).getElementsByTag("a").text();
                String author = elements1.get(i).getElementsByTag("h2").text();
-//               String price = ((elements2.get(i).getElementsByTag("span").text()).replaceAll("₫","").trim());
+               String temp = ((elements2.get(i).getElementsByTag("span").text()).replaceAll("₫","").trim());
+               double price =  Double.parseDouble(temp.replaceAll(",", ""));
                String image = elements4.get(i).getElementsByTag("img").attr("src");
-//               Book book = new Book(id, name, price);
-//                System.out.println(book);
-                System.out.println(image);
+                Book book = new Book(id, code, name,author_id, price, category_id, image, description);
+                bookArrayList.add(book);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void main(String[] args) {
-        CrawlsDataBoos crawlsDataBoos = new CrawlsDataBoos();
-        crawlsDataBoos.getData();
-    }
+//    public static void main(String[] args) {
+//        CrawlsDataBoos crawlsDataBoos = new CrawlsDataBoos();
+//        crawlsDataBoos.getData();
+//    }
 }
