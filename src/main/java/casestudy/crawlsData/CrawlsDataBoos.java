dfddf5bc24b634bbfd8b5add23944e4ded1d2e6a
ishopjapan <thoitran2107@gmail.com>
2022-08-30 10:02:52 +0700

    sua cao

--- src/main/java/casestudy/crawlsData/CrawlsDataBoos.java
+++ src/main/java/casestudy/crawlsData/CrawlsDataBoos.java
@@ -16,7 +16,7 @@ public class CrawlsDataBoos {
             ArrayList<Element> elements = document.getElementsByClass("t-view");
             ArrayList<Element> elements1 = document.getElementsByClass("au-view");
             ArrayList<Element> elements2 = document.getElementsByClass("real-price");
-            ArrayList<Element> elements3 = document.getElementsByClass("collection-title");
+//            ArrayList<Element> elements3 = document.getElementsByClass("collection-title");
             ArrayList<Element> elements4 = document.getElementsByClass("img-view");
 //            ArrayList<Element> elements5 = document.getElementsByClass("status");
             int id = 0;
