8f37157ac9d08a3e9f13c19203c64188bb804fb6
ishopjapan <thoitran2107@gmail.com>
2022-08-30 11:04:38 +0700

    feix git

--- src/main/java/casestudy/crawlsData/addDataBase.java
+++ src/main/java/casestudy/crawlsData/addDataBase.java
@@ -1,7 +1,5 @@
 package casestudy.crawlsData;
 
-import casestudy.config.ConnectionJDBC;
-
 public class addDataBase {
     private CrawlsDataBoos crawlsDataBoos = new CrawlsDataBoos();
     private ConnectionJDBC connectionJDBC = new ConnectionJDBC();
