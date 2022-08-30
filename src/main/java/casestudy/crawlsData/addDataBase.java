0be895e567de334f6905b7c14bbd77ac400567aa
ishopjapan <thoitran2107@gmail.com>
2022-08-30 10:36:18 +0700

    add crawl

---
+++ src/main/java/casestudy/crawlsData/addDataBase.java
@@ -0,0 +1,8 @@
+package casestudy.crawlsData;
+
+import casestudy.config.ConnectionJDBC;
+
+public class addDataBase {
+    private CrawlsDataBoos crawlsDataBoos = new CrawlsDataBoos();
+    private ConnectionJDBC connectionJDBC = new ConnectionJDBC();
+}
