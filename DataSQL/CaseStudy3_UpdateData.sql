5b21c88fb883dfd7c067cff9cb35799cce3b64ce
ishopjapan <thoitran2107@gmail.com>
2022-08-31 14:55:39 +0700

    update data

--- DataSQL/CaseStudy3_UpdateData.sql
+++ DataSQL/CaseStudy3_UpdateData.sql
@@ -2,33 +2,8 @@ use casestudy3;
 # Chạy dòng lệnh này để đổi tên cột imgage
 ## alter table books rename column imgage to image;
 alter table books rename column author_id to author;
-alter table books modify column author varchar(255);
+
 # Book:
-select * from books;
-
-insert into books values (1,'SGK','Học cùng bé',1,123,1,'','');
-insert into books values (2,'SGK1','Học cùng bé 1',2,1234,1,'','');
-delete from books where id = 1;
-delete from books where id = 2;
-# Author:
-select * from author;
-insert into author values (1,'yolo','2000-08-12','Viet Nam','');
-insert into author values (2,'yolo2','2000-08-12','Viet Nam','');
-# Category:
-select * from category;
-insert into category values (1,'Khoa học viễn tưởng','Thể loại hành động không có thực');
-# Customer:
-select * from customer;
-select * from customer where name like '%duc%';
-insert into customer values (1,'bay',18,'Nam','HN','012345678','abcxyz@gmail.com','bay','123456','2000-08-12');
-# Orders:
-select * from orders;
-insert into orders values (1,1,'2022-08-30');
-select * from ordersdetail;
-alter table books drop constraint books_ibfk_1;
-alter table books drop constraint books_ibfk_2;
-alter table books drop column category_id;
-
 select * from books;
 insert into books(id, code, name, author, price, image, description) VALUE (1, 'AA1', 'Khong gia dinh(tai ban nam 2022)', 'Hector Malot', 127.500, 'https://bookbuy.vn/Res/Images/Product/khong-gia-dinh-tai-ban-2022-_118548_1.jpg', 'Sach kha hay va doc dao');
 insert into books(id, code, name, author, price, image, description) VALUE (2, 'AA2', 'Dao giau vang(tai ban nam 2022)', 'R.L.Stevenson', 38.500, 'https://bookbuy.vn/Res/Images/Product/dao-giau-vang-tac-pham-chon-loc-van-hoc-scotland-tai-ban-2022-_118540_1.jpg', 'Sach cho nguoi lon');
@@ -38,5 +13,38 @@ insert into books(id, code, name, author, price, image, description) VALUE (5, '
 insert into books(id, code, name, author, price, image, description) VALUE (6, 'AA6', 'Khong sao dau con, chap nhan loi tu choi', 'Tran Hong Tuan', 28.500, 'https://bookbuy.vn/Res/Images/Product/khong-sao-dau-con-chap-nhan-loi-tu-choi_116732_1.PNG', 'Sach cho moi lua tuoi');
 insert into books(id, code, name, author, price, image, description) VALUE (7, 'AA7', 'Chuyen ve chu tho cool nhat Ha Lan', 'Guido Van Genechten', 44.500, 'https://bookbuy.vn/Res/Images/Product/chuyen-ve-chu-tho-cool-nhat-ha-lan-riki-gan-da_115849_1.jpg', 'Sach kha hay va doc dao');
 insert into books(id, code, name, author, price, image, description) VALUE (8, 'AA8', 'Chuyen nay chuyen kia', 'Dom Dom', 89.500, 'https://bookbuy.vn/Res/Images/Product/115376_chuyen-nay-chuyen-kia-2-ban-dac-biet_115375_2.png', 'Sach cho tre em');
+alter table books drop constraint books_ibfk_1;
+alter table books drop constraint books_ibfk_2;
+alter table books drop column category_id;
+alter table books modify column author varchar(255);
+
+# Author:
+select * from author;
+insert into author values (1,'yolo','2000-08-12','Viet Nam','');
+insert into author values (2,'yolo2','2000-08-12','Viet Nam','');
+# Category:
+select * from category;
+insert into category values (1,'Khoa học viễn tưởng','Thể loại hành động không có thực');
+delete from category where id = 1;
+insert into category(id, type, description) VALUE (1, 'Chinh tri - phap luat', 'danh cho nhung chinh tri gia');
+insert into category(id, type, description) VALUE (2, 'Khoa hoc cong nghe - Kinh te', 'danh cho hoc sinh, sinh vien, ...');
+insert into category(id, type, description) VALUE (3, 'Van hoc nghe thuat', 'danh cho nhung nguoi dam me nghe thuat');
+insert into category(id, type, description) VALUE (4, 'Van hoa hoi - lich su', 'danh cho nhung nguoi yeu lich su');
+insert into category(id, type, description) VALUE (5, 'Giao trinh', 'danh cho hoc sinh, sinh vien');
+insert into category(id, type, description) VALUE (6, 'Truyen, tieu thuyet', 'danh cho nhung nguoi dam me truyen');
+insert into category(id, type, description) VALUE (7, 'Tam ly, tam linh, ton giao', 'danh cho nhung nguoi yeu tim');
+insert into category(id, type, description) VALUE (8, 'Thieu nhi', 'danh cho tre con');
+# Customer:
+select * from customer;
+select * from customer where name like '%duc%';
+insert into customer values (1,'bay',18,'Nam','HN','012345678','abcxyz@gmail.com','bay','123456','2000-08-12');
+# Orders:
+select * from orders;
+insert into orders values (1,1,'2022-08-30');
+select * from ordersdetail;
+
+
+
+
 
 
